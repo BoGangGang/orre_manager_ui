f36e6c03f975e052f07aea27672b384aef8a1ae1
BoGangGang <ssg020910@naver.com>
2024-05-01 14:25:59 +0900

    로그인 UI 수정

--- lib/presenter/login_screen.dart
+++ lib/presenter/login_screen.dart
@@ -42,86 +42,110 @@ class _LoginScreenBody extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     return Scaffold(
-      body: Padding(
-        padding: EdgeInsets.all(20.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Padding(
-              padding: EdgeInsets.all(20),
-              child: Text(
-                "로그인",
-                style: TextStyle(
-                  fontFamily: 'Dovemayo_gothic',
-                  fontSize: 32,
-                  color: Colors.white,
-                ),
+      backgroundColor: Colors.white,
+      body: Stack(
+        children: [
+          Image.asset(
+            "assets/images/waveform/wave_shadow.png",
+            width: MediaQuery.of(context).size.width,
+            fit: BoxFit.cover,
+          ),
+          Image.asset(
+            "assets/images/waveform/wave.png",
+            width: MediaQuery.of(context).size.width,
+            fit: BoxFit.cover,
+          ),
+          Padding(
+            padding: EdgeInsets.all(20),
+            child: Text(
+              "로그인",
+              style: TextStyle(
+                fontFamily: 'Dovemayo_gothic',
+                fontSize: 32,
+                color: Colors.white,
               ),
             ),
-            Image.asset(
-              "assets/images/waveform/wave_shadow.png",
-              width: MediaQuery.of(context).size.width,
-              fit: BoxFit.cover,
-            ),
-            Image.asset(
-              "assets/images/waveform/wave.png",
-              width: MediaQuery.of(context).size.width,
-              fit: BoxFit.cover,
-            ),
-            TextField(
-              controller: _idController,
-              decoration: InputDecoration(
-                labelText: '전화번호',
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(10.0),
-                  borderSide: BorderSide(
-                    color: Color(0xFF77AAD8),
-                    width: 3.0,
+          ),
+          SizedBox(height: 20),
+          Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                    child: TextField(
+                      controller: _idController,
+                      decoration: InputDecoration(
+                        labelText: '전화번호',
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10.0),
+                          borderSide: BorderSide(
+                            color: Color(0xFF77AAD8),
+                            width: 3.0,
+                          ),
+                        ),
+                        labelStyle: TextStyle(
+                          fontFamily: 'Dovemayo_gothic',
+                          fontSize: 20,
+                          color: Color(0xFFD9D9D9),
+                        ),
+                      ),
+                    ),
+                    width: MediaQuery.of(context).size.width * 0.9),
+                SizedBox(height: 20),
+                Container(
+                    child: TextField(
+                      controller: _pwController,
+                      decoration: InputDecoration(
+                        labelText: '비밀번호',
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10.0),
+                          borderSide: BorderSide(
+                            color: Color(0xFF77AAD8),
+                            width: 3.0,
+                          ),
+                        ),
+                        labelStyle: TextStyle(
+                          fontFamily: 'Dovemayo_gothic',
+                          fontSize: 20,
+                          color: Color(0xFFD9D9D9),
+                        ),
+                      ),
+                    ),
+                    width: MediaQuery.of(context).size.width * 0.9),
+                SizedBox(height: 20),
+                ElevatedButton(
+                  onPressed: () => loginProcess(ref),
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Color(0xFF72AAD8),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    minimumSize:
+                        Size(MediaQuery.of(context).size.width * 0.9, 60),
+                  ),
+                  child: Text(
+                    "로그인",
+                    style: TextStyle(
+                      fontFamily: 'Dovemayo_gothic',
+                      fontSize: 24,
+                      color: Colors.black,
+                    ),
                   ),
                 ),
-                labelStyle: TextStyle(
-                  fontFamily: 'Dovemayo_gothic',
-                  fontSize: 20,
-                  color: Color(0xFFD9D9D9),
-                ),
-              ),
+                SizedBox(height: 20),
+              ],
             ),
-            SizedBox(height: 20),
-            TextField(
-              controller: _pwController,
-              decoration: InputDecoration(
-                labelText: '비밀번호',
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(10.0),
-                  borderSide: BorderSide(
-                    color: Color(0xFF77AAD8),
-                    width: 3.0,
-                  ),
-                ),
-                labelStyle: TextStyle(
-                  fontFamily: 'Dovemayo_gothic',
-                  fontSize: 20,
-                  color: Color(0xFFD9D9D9),
-                ),
-              ),
-              obscureText: true,
+          ),
+          Positioned(
+            left: 0,
+            right: 0,
+            bottom: 0,
+            child: Container(
+              height: 20,
+              color: Color(0xFF72AAD8),
             ),
-            SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () => loginProcess(ref),
-              child: Text('Login'),
-            ),
-            Positioned(
-              left: 0,
-              right: 0,
-              bottom: 0,
-              child: Container(
-                height: 20,
-                color: Color(0xFF72AAD8),
-              ),
-            ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
@@ -131,9 +155,6 @@ class _LoadingScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Login Screen'),
-      ),
       body: Center(
         child: CircularProgressIndicator(),
       ),
